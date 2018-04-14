fefec8499954265c0dbe88928cec3bf6c4d0eea0
DaffyNinja <marcusday117@gmail.com>
2018-04-14 11:56:19 +1000

    Made camera control and shuttle movement scripts and added them to camera and shuttle object
    Made shuttle object a prefab

---
+++ Final Transmission Project/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour {
+
+    public float zoomSpeed;
+
+    public float zoomMax;
+
+    public bool canZoom;
+
+    Camera cam;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        cam = GetComponent<Camera>();
+
+        canZoom = true;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (canZoom)
+        {
+            cam.orthographicSize += zoomSpeed;
+        }
+
+        if (cam.orthographicSize >= zoomMax)
+        {
+            canZoom = false;
+        }
+
+		
+	}
+}
---
+++ Final Transmission Project/Assets/Scripts/ShuttleMovement.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShuttleMovement : MonoBehaviour {
+
+    [Header("Rotation")]
+    public bool canRotate;
+    public float rotateSpeed;
+
+    [Header("Move")]
+    public bool canMove;
+    public float movespeed;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        canRotate = true;		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (canRotate)
+        {
+            transform.Rotate(0, 0, rotateSpeed);
+        }
+
+        if (canMove)
+        {
+        }
+		
+	}
+}
